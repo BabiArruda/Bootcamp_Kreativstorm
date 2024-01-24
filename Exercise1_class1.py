c11cc1a3c10c0fdf245881d4d4f9e3461bfbe1f9
Barbara Arruda <95.bazinha@gmail.com>
2024-01-24 10:56:36 +0000

    First exercise of class one corrected

---
+++ Exercise1_class1.py
@@ -0,0 +1,13 @@
+# 1. Write a program to prompt the user for hours and rate
+# per hour to compute gross pay.
+
+# Example:
+# Enter Hours: 35
+# Enter Rate: 2.75
+
+# Result:
+# Pay: 96.25
+
+hours = float(input('How many hours you work?\n'))
+rate = float(input('Enter your rate:\n'))
+print(format('Your salary is going to be: ', hours*rate, ".2f"))
